befbd31a06e17b94b71b4de562037673403f516d
Berkan Duzgun <berkan.duzgun@aizetech.nl>
2026-01-25 23:34:24 +0300

    Setup the initial website

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Aize';
+export const SITE_DESCRIPTION = 'Technical architecture for systems that can\'t afford wrong decisions. Writing about trade-offs, platform choices, and durable system design.';
